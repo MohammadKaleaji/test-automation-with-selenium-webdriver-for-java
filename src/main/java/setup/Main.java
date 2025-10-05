4fca2f14ab68dc5bb100ccd956dafc40a8a29e42
MohammadKaleaji <mohammadkaleaji@gmail.com>
2025-10-05 21:42:56 +0300

    chellenge: complete a form

--- src/main/java/setup/Main.java
+++ src/main/java/setup/Main.java
@@ -24,15 +24,55 @@ public class Main {
 
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
+    // Enter the user name
+    WebElement userName = driver.findElement(By.id("my-text-id"));
+    userName.sendKeys("Kaleaji");
+
+    // Enter the password
+    WebElement password = driver.findElement(By.name("my-password"));
+    password.sendKeys("123456");
+
+    // Enter the text area
+    WebElement textArea = driver.findElement(By.name("my-textarea"));
+    textArea.sendKeys("I am learning Selenium Webdriver!");
+    
+    // Select the dropdown
     Select dropdown = new Select(driver.findElement(By.name("my-select")));
-    dropdown.selectByValue("2");
-    //dropdown.selectByVisibleText("Three");
+    dropdown.selectByValue("3");
+    
+    // Check the checkbox
+    WebElement checkbox = driver.findElement(By.id("my-check-1"));
+    if (checkbox.isSelected()) {
+      checkbox.click(); // Uncheck the checkbox if it is already checked
+    }
+
+    // Uncheck the checkbox
+    WebElement checkbox2 = driver.findElement(By.id("my-check-2"));
+    if (!checkbox2.isSelected()) {
+      checkbox2.click(); // Check the checkbox if it is not checked
+    }
+
+    // Check the radio button
+    WebElement radioButton = driver.findElement(By.id("my-radio-2"));
+    if (!radioButton.isSelected()) {
+      radioButton.click(); // Check the radio button if it is not checked
+    }
+
+    try {
+      Thread.sleep(8000);
+    } catch (InterruptedException e) {
+    }
+
+    // Click the submit button
+    WebElement submitButton = driver.findElement(By.tagName("button"));
+    submitButton.click();
 
     try {
       Thread.sleep(5000);
     } catch (InterruptedException e) {
     }
 
+    // Quit the driver
     driver.quit();
   }
 }
